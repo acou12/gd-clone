14fcfe929cfb13de8125a3490a72da818578897a
acou12 <ayden.coughlin@gmail.com>
2019-06-11 21:15:52 +0900

    Setup processing methods

---
+++ src/Game.java
@@ -0,0 +1,23 @@
+import processing.core.PApplet;
+
+public class Game extends PApplet {
+	public static void main(String... args) {
+		Game g = new Game();
+		PApplet.runSketch(new String[]{"Geometry Dash"}, g);
+	}
+	
+	@Override
+	public void settings() {
+		
+	}
+	
+	@Override
+	public void setup() {
+		
+	}
+	
+	@Override
+	public void draw() {
+		
+	}
+}
